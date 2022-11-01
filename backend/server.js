cb7606ac5ba6c0340deccb33a233b8305f0dca0c
Chris L <chris.ladden@gmail.com>
2022-11-01 10:59:07 -0700

    fixed build path

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ mongoose.connect(process.env.MONGO_URL, () => {
 //Heroku attachment
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, '..', 'build'));
+  app.use(express.static("frontend/build"));
   app.use(express.static('public'));
   app.get("*", (req, res) => {
-  const index = path.join(__dirname, '..', 'build', 'index.html');
+  const index = path.join(__dirname , "../frontend/build", "index.html");
   res.sendFile(index);
   });
 }
