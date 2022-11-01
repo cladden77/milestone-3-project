f6b7855c66843b8ac23840c8651272fae607855b
Chris L <chris.ladden@gmail.com>
2022-11-01 15:25:20 -0700

    fixed cors issue

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,14 @@ mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connected to MongoDB");
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(cors());
 
 //Heroku attachment
