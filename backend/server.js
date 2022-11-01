13711baf6502bd3ad3d9924c98a7de56ce089259
Chris L <chris.ladden@gmail.com>
2022-11-01 10:18:36 -0700

    fixed build path

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,8 @@ mongoose.connect(process.env.MONGO_URL, () => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("backend/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 }
 
 // Listen for Connections
