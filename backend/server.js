8d8c4d4a7a6e64e36a84311079b83aa79e722d33
Chris L <chris.ladden@gmail.com>
2022-11-01 11:33:33 -0700

    fixed syntax

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static('public'));
   app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
-});
-  res.sendFile(index);
+    res.sendFile(index);
+    });
   }
 
 // Listen for Connections
