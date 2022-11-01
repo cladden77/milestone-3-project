e5e0f90cf426816474facf4a9667c0dbef66e42d
Chris L <chris.ladden@gmail.com>
2022-11-01 10:12:39 -0700

    adjusted production path

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URL, () => {
 //Heroku attachment
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("backend/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
